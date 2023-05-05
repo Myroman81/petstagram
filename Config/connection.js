90d3019083f3031f29a432b147130ec24408c414
myroman-wuLtCL <myroman@protonmail.com>
2023-05-05 13:12:57 -0400

    updated connection.js

--- Config/connection.js
+++ Config/connection.js
@@ -1,16 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+let sequelize;
 
-const sequelize = new Sequelize(
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.jawsdb_url);
+} else {
+  const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
-    }
+    },
   );
-
+}
 
 module.exports = sequelize;
